d7e615b48b8a770aa9611fd66513828444206e48
niharika2k00 <dniharika16@gmail.com>
2021-11-06 21:39:40 +0530

    setup frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+const App = () => {
+
+
+  return (
+    <Router>
+      <main >
+        {/* <Route path='/' component={ABCD} /> */}
+        PROJECT STARTING  :)
+
+
+
+      </main>
+    </Router>
+  )
+}
+
+export default App;
