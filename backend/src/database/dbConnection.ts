4687bff4ff27464dab92daffc4dcf50f009b41fd
Ankur Saha <ankursahamoto@gmail.com>
2021-11-06 15:05:14 +0530

    sql functions changed to async and await

--- backend/src/database/dbConnection.ts
+++ backend/src/database/dbConnection.ts
@@ -1,12 +1,6 @@
 import e from 'express';
 import mysql from 'mysql2/promise';
 
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'ankur12345',
-//   database: 'examsimulation',
-// });
 let db: any;
 const connectDatabase = async () => {
   db = await mysql.createConnection({
@@ -22,13 +16,6 @@ const connectDatabase = async () => {
 const getDb = () => {
   return db;
 };
-// const db = async () => {
-//   return await mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'ankur12345',
-//     database: 'examsimulation',
-//   });
-// };
+
 export default db;
 export { connectDatabase, getDb };
