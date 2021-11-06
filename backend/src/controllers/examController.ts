0244ec046bfe847ba3a3ecffa81bb9b8d6bf6b7c
Ankur Saha <ankursahamoto@gmail.com>
2021-11-06 13:58:30 +0530

    table creation routes added

--- backend/src/controllers/examController.ts
+++ backend/src/controllers/examController.ts
@@ -1,6 +1,8 @@
 import catchAsync from '../utils/catchAsync';
 import { Response, Request, NextFunction } from 'express';
 import db from '../database/dbConnection';
+import scheduler from 'node-cron';
+import { v4 as uuid } from 'uuid';
 
 export const createExamTable = catchAsync(
   async (req: Request, res: Response) => {
@@ -15,3 +17,42 @@ export const createExamTable = catchAsync(
     });
   }
 );
+
+export const createExam = catchAsync(async (req: Request, res: Response) => {
+  //data =>{examData :{ subject,questions,startTime,duration },teacherId:String}
+  let query =
+    'insert into `Exam` (`id`,`subject`,`questions`,`startTime`,`duration`) values(?,?,?,?,?)';
+  let data = req.body.examData || {};
+  data['id'] = uuid();
+  db.query(
+    query,
+    [
+      data.id,
+      data.subject,
+      JSON.stringify(data.questions),
+      new Date(),
+      data.duration,
+    ],
+    (err, result) => {
+      if (err) {
+        res.status(500).send('Exam not created !');
+        console.log(err);
+      } else {
+        res.status(200).send('Exam Created !');
+        console.log(result);
+        //configure schedulers
+      }
+    }
+  );
+  console.log([
+    data.id,
+    data.subject,
+    data.questions,
+    data.startTime,
+    data.duration,
+  ]);
+});
+
+const evaluateExam = (examId: String) => {
+  //evaluate Exam and update database
+};
