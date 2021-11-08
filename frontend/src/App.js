d97f3f5adfbec1fa609362a495f27e48ac0785fd
Ankur Saha <ankursahamoto@gmail.com>
2021-11-08 16:39:48 +0530

    browser router fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,17 @@
-
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Redirect, useHistory, Switch, useLocation, } from "react-router-dom";
+import { Switch, Route } from 'react-router-dom';
 import CREATE_EXAM from './Pages/ExamCreation/ExamCreation.jsx';
 
-
 const App = () => {
-
-
   return (
     <Switch>
       {/* <main > */}
       {/* <Route path='/' component={ABCD} /> */}
-      PROJECT STARTING  :)
-      <Route path='/' component={CREATE_EXAM} />
-
+      PROJECT STARTING :)
+      <Route path="/" component={CREATE_EXAM} />
       {/* </main> */}
     </Switch>
-  )
-}
+  );
+};
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,14 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
