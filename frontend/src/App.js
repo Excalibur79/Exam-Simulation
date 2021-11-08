f911ece725bf507e751429bc15b24d8d82ca5ce8
niharika2k00 <dniharika16@gmail.com>
2021-11-08 21:48:19 +0530

    exam creation pg ongoing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
+
 import React, { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import CREATE_EXAM from './Pages/ExamCreation/ExamCreation.jsx';
+import CreateExam from './Pages/ExamCreation/ExamCreation.jsx';
 
 const App = () => {
   return (
     <Switch>
-      {/* <main > */}
       {/* <Route path='/' component={ABCD} /> */}
-      PROJECT STARTING :)
-      <Route path="/" component={CREATE_EXAM} />
-      {/* </main> */}
+      {/* PROJECT STARTING :) */}
+      <Route path="/create-exam" component={CreateExam} />
+
     </Switch>
   );
 };
--- frontend/src/Pages/ExamCreation/ExamCreation.jsx
+++ frontend/src/Pages/ExamCreation/ExamCreation.jsx
@@ -1,35 +1,201 @@
 
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@mui/styles';
-// import { useHistory, useParams } from 'react-router-dom';
+import { TextField, Grid, Paper, Box, styled } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import { DateTimePicker, LocalizationProvider, DesktopDatePicker, TimePicker } from '@mui/lab';
+// import DateAdapter from '@mui/lab/AdapterMoment';
+// import DateAdapter from '@mui/lab/AdapterDateFns';
+// import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { useHistory, useParams } from 'react-router-dom';
+import MarkdownIt from 'markdown-it'
+import MdEditor from 'react-markdown-editor-lite'
+import 'react-markdown-editor-lite/lib/index.css';
+import AddToPhotosIcon from '@mui/icons-material/AddToPhotos';
+import './examCreation.css';
 
 
 const useStyles = makeStyles({
     root: {
-        // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        // border: 0,
-        // borderRadius: 3,
-        // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        // color: 'white',
-        // height: 48,
-        // padding: '0 30px',
+        padding: "1.6rem",
+        margin: "1rem",
+        fontSize: "1.6rem"
+    },
+    heading: {
+        fontSize: "3rem",
+        textAlign: "center",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    editor: {
+        marginTop: "2rem",
+        paddingTop: "2rem",
+    },
+    addQuestionBar: {
+        paddingTop: "2rem",
+        height: "2rem",
     },
 });
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+var count = 0;
 
 const ExamCreation = () => {
 
     const classes = useStyles();
-    // const history = useHistory();
+    const history = useHistory();
+    const mdParser = new MarkdownIt();
 
-    console.log("hello");
+    const [htmlText, setHtmlText] = useState();
+    const [value, setValue] = useState(new Date('2014-08-18T21:11:54'));
+    const [add, setAdd] = useState(0);
+
+    const handleChange = (newValue) => {
+        setValue(newValue);
+    };
+
+    const ImageUploadHandler = async (file, callback) => {
+        console.log("file : ", file);
+        // const Picture = await uploadImg(file);
+        // console.log(Picture);
+        // callback(Picture);
+    }
+
+    //  EDITOR ON CHANGE  HANDLER (runs for a single change in the editor)  
+    function EditorChangeHandler({ html, text }) {
+        console.log('HTML text = ', html)
+        // console.log('Mark Down text = ', text)
+        // document.getElementById("blogBody").innerHTML = html;
+        setHtmlText(html);
+        // document.getElementById('blogBody').querySelector('img').classList.add("imgStyle");
+        // var imageTag = document.getElementById('blogBody').querySelectorAll('img');
+        // console.log(imageTag);
+        // for (var i = 0; i < imageTag.length; ++i) {
+        // imageTag[i].classList.add('imgStyle');                          // apply styling to the pic using js when render in the same page
+        // }
+    }
+
+    const AddQuestionHandler = () => {
+        console.log("helloasdfaf");
+        {
+            (
+                <div className={classes.editor}>
+                    <MdEditor
+                        style={{ height: "36rem" }}
+                        renderHTML={(text) => mdParser.render(text)}
+                        onChange={EditorChangeHandler}
+                        onImageUpload={ImageUploadHandler}
+                        placeholder={"Start framing question..."}
+                    />
+                </div>)
+        }
+    }
+
+    useEffect(() => {
+        if (add) {
+            return (
+                <div className={classes.editor}>
+                    <MdEditor
+                        style={{ height: "36rem" }}
+                        renderHTML={(text) => mdParser.render(text)}
+                        onChange={EditorChangeHandler}
+                        onImageUpload={ImageUploadHandler}
+                        placeholder={"Start framing question..."}
+                    />
+                </div>
+            )
+        }
+    }, [add]);
+
+
+
 
     return (
         <div className={classes.root} >
-            The error prop toggles the error state. The helperText prop can then be used to provide feedback to the user about the error.The error prop toggles
-            the error state. The helperText prop can then be used to provide feedback to the user about the error.The error prop toggles the error state. The
-            helperText prop can then be used to provide feedback to the user about the error.The error prop toggles the error state. The helperText prop can
-            then be used to provide feedback to the user about the error.
-        </div>
+            <p className={classes.heading} > University of Engineering & Management</p>
+
+            <p> Full Marks : 20  </p>
+            <div>
+                <TextField
+                    // id="standard-password-input"
+                    // label="Password"
+                    // type="password"
+                    // autoComplete="current-password"
+                    // variant="standard"
+
+
+                    required
+                    id="standard-required"
+                    label="Subject"
+                    // defaultValue="Hello World"
+                    variant="standard"
+                    fullWidth={true}
+                    inputProps={{ style: { fontSize: '1.9rem' } }}
+                    style={{ marginTop: '0.5rem', width: '22rem' }}
+                // InputProps={{ fontSize: '2rem' }}
+                // classes={{ fontSize: '2rem' }}
+                />
+
+
+                {/* <LocalizationProvider dateAdapter={AdapterDateFns} >
+                    <Stack spacing={3}>
+                        <DesktopDatePicker
+                            label="Date desktop"
+                            inputFormat="MM/dd/yyyy"
+                            value={value}
+                            onChange={handleChange}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                        <TimePicker
+                            label="Time"
+                            value={value}
+                            onChange={handleChange}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                        <DateTimePicker
+                            label="Date&Time picker"
+                            value={value}
+                            onChange={handleChange}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </Stack>
+                </LocalizationProvider> */}
+
+            </div>
+
+
+            <div style={{ marginBottom: "9rem" }} >
+                <Box lg={{ flexGrow: 1 }}>
+                    <Grid container spacing={2} className={classes.addQuestionBar} >
+                        <Grid item xs={12} lg={12} md={12} >
+                            <Item style={{ fontSize: "2rem" }} onClick={() => setAdd(count++)} >
+                                <AddToPhotosIcon fontSize="large" /> Add Question
+                            </Item>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </div>
+
+            <div className={classes.editor}>
+                <MdEditor
+                    style={{ height: "36rem" }}
+                    renderHTML={(text) => mdParser.render(text)}
+                    onChange={EditorChangeHandler}
+                    onImageUpload={ImageUploadHandler}
+                    placeholder={"Start framing question..."}
+                />
+            </div>
+
+
+
+
+        </div >
     )
 }
 
