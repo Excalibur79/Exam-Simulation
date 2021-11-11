f598f66d748a77fa6a247a3b9ae31218f135b3ec
Subhankar Pal <developer.subho57@gmail.com>
2021-11-11 12:15:06 +0530

    Db migrated

--- backend/src/database/dbConnection.ts
+++ backend/src/database/dbConnection.ts
@@ -4,10 +4,10 @@ import mysql from 'mysql2/promise';
 let db: any;
 const connectDatabase = async () => {
   db = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'ankur12345',
-    database: 'examsimulation',
+    host: 'mydbinstance.cyfjjout6pho.ap-south-1.rds.amazonaws.com',
+    user: 'subho57',
+    password: 'adminDBpassword123',
+    database: 'examSimulation',
   });
   if (db) console.log('Database Connected !');
   else console.log('Database Not Connected !');
